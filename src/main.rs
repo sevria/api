e81542b143d66ac1323ff4ccfa54b51ae04c9133
Ukasyah <hi.ukasyah@gmail.com>
2025-07-20 12:53:10 +0700

    add axum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use axum::{Router, response::Html, routing::get};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let app = Router::new().route("/", get(handler));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:4000").await?;
+
+    println!("listening on {}", listener.local_addr()?);
+
+    axum::serve(listener, app).await?;
+
+    Ok(())
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello from Sevria API!</h1>")
 }
